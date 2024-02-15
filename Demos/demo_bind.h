3b941f918b053026bc5d74c1ba07139e5886e62e
Sunil Pandey <inkingmind@gmail.com>
2024-02-16 00:37:26 +0530

    more demo added to bind func

--- Demos/demo_bind.h
+++ Demos/demo_bind.h
@@ -19,9 +19,17 @@ int demoBind()
     answer = funPow2(200, 30);
     assert(answer == 70);
 
-    funPow2 = std::bind(ly_add, placeholders::_1, placeholders::_2, 5);
-    answer = funPow2(10, 1);
+    funPow2 = std::bind(ly_add, placeholders::_1, placeholders::_2, 5); 
+    answer = funPow2(10, 1); 
     assert(answer == 4);
+
+
+    auto funPow3 = std::bind(ly_add, 20, placeholders::_2, 5); //20 and 5 are hardcoded as first/third arg
+    answer = funPow3(10, 1); //placeholders::_2 means 1 woudl be passed as second arg, placeholders::_1 means 10 would be passed as seond arg of funPow2 fun in above line
+    
+    auto funPow4 = std::bind(ly_add, placeholders::_2, placeholders::_1, 5); 
+    answer = funPow4(10, 1); //  Here 1 would be passed as first and 10 would be passed as second arg 
+
 
     return 0;
 }
