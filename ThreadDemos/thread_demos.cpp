86470b92c8540cc0f55395afeee6220f6c2009a0
Sunil Pandey <inkingmind@gmail.com>
2024-09-25 12:51:30 +0530

    demo added for diamond prob in cpp multi-inheritance

--- ThreadDemos/thread_demos.cpp
+++ ThreadDemos/thread_demos.cpp
@@ -2,34 +2,10 @@
 #include "demo_threads.h"
 using namespace std;
 
-class Base
-{
-
-public:
-	virtual void fun(int i = 10) {
-		cout << i;
-	}
-};
-
-class Derived : public Base
-{
-public:
-	virtual void fun(int i = 20) {
-		cout << i;
-	}
-};
-
-
-
 int main()
 {
-	Base* p = new Derived;
-	p->fun();
+	cout << "thread demos!!" << endl;
+	ns_thread::demo();
 
 	return 0;
-	
-	//cout << "thread demos!!" << endl;
-	//ns_thread::demo();
-
-	//return 0;
 }
